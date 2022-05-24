8359a5c976333713892d99f834c443fda7964f29
awo.ali <awoali@hotmail.co.uk>
2022-05-24 16:23:38 +0100

    Added confetti feature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 import "./style.scss";
 
-console.log("Welcome to NPM!");
+// Named import - confetti -> bringing the package into our project
+
+import confetti from "canvas-confetti";
+
+// readthe docs/options
+
+const confettiClick = document.getElementById("button");
+
+const dogImage = document.getElementById("dog-image");
+
+const newInput = document.getElementById("URL-Input")
+console.log(newInput);
+
+console.log(dogImage);
+console.log(confettiClick);
+
+const clickConfetti = () => {
+    
+    confetti({
+        particleCount: 150
+      });;
+}
+
+
+/*you can add an object to the function -- const confettiOptions = 
+{
+
+}*/
+
+
+confettiClick.addEventListener("click", clickConfetti)
